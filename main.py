3375e81202e0f5bcc026735a87089a794add7a53
Taleb Damaree <tdamaree18@gmail.com>
2024-02-19 11:32:21 -0500

    updates

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import pandas as pd
 
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+
 ny_house_dataset_master_df = pd.read_csv('NY-House-Dataset.csv')
+
+print(ny_house_dataset_master_df.head())
 
 condo_df = pd.DataFrame()
 broker_title = ny_house_dataset_master_df.iloc[:,1]
