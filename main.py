a293f85e9452b1e36bd0348bc681e159a8e1d22c
Taleb Damaree <tdamaree18@gmail.com>
2024-02-19 10:25:34 -0500

    updates

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ ny_house_dataset_master_df = pd.read_csv('NY-House-Dataset.csv')
 condo_df = pd.DataFrame()
 broker_title = ny_house_dataset_master_df.iloc[:,0]
 
-condo_df.to_csv('condo_df', index=False)
+condo_df.to_csv('condo_df.csv', index=False)
