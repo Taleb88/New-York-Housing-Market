068f387c133d15eac4c9d3af63b3baebba1f755c
Taleb Damaree <tdamaree18@gmail.com>
2024-02-19 11:09:29 -0500

    created condos dataframe

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import pandas as pd
 ny_house_dataset_master_df = pd.read_csv('NY-House-Dataset.csv')
 
 condo_df = pd.DataFrame()
-broker_title = ny_house_dataset_master_df.iloc[:,0]
+broker_title = ny_house_dataset_master_df.iloc[:,1]
+condo_df['Broker Title'] = broker_title.copy()
 
 condo_df.to_csv('condo_df.csv', index=False)
